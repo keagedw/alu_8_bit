9735bb826f5869fabc1f6a4fd018537e361e779f
Keagan Edward Wangsakentjana <keagedw.official@gmail.com>
2026-06-11 01:29:38 +0800

    Increase carry_lookahead adder from 4 bits to 8 bits and took out carry_in

--- sim/carry_lookahead_test.v
+++ sim/carry_lookahead_test.v
@@ -1,32 +1,32 @@
-`timescale 1ns/1ps
+`timescale 100ps/1ps
 
 module carry_lookahead_test;
 
-reg  [3:0] a;
-reg  [3:0] b;
-reg        carry_in;
-wire [3:0] result;
+reg  [7:0] a;
+reg  [7:0] b;
+wire [7:0] result;
 wire       carry_out;
 
 carry_lookahead test(
     .a(a),
     .b(b),
-    .carry_in(carry_in),
     .result(result),
     .carry_out(carry_out)
 );
 
 initial begin
-    // zero test
-    a = 4'd0; b = 4'd0; carry_in = 1'b0; #2;
-    // no carry_out test
-    a = 4'd3; b = 4'd4; carry_in = 1'b0; #2;
-    a = 4'd1; b = 4'd14; carry_in = 1'b0; #2;
-    // carry_out test
-    a = 4'd13; b = 4'd8; carry_in = 1'b0; #2;
-    // carry_in test
-    a = 4'd0; b = 4'd0; carry_in = 1'b1; #2;
-    a = 4'd4; b = 4'd5; carry_in = 1'b1; #2;
+    // test ADD
+    a = 8'd170; b = 8'd76; #3;
+    a = 8'd56; b = 8'd105; #3;
+    // test with 0
+    a = 8'd0; b = 8'd0; #3;
+    a = 8'd29; b = 8'd0; #3;
+    a = 8'd56; b = 8'd93; #3;
+    // test carry_out
+    a = 8'd255; b = 8'd0; #3;
+    a = 8'd255; b = 8'd1; #3;
+    // test overflow
+    a = 8'd200; b = 8'd200; #3;
 end
 
 endmodule
--- src/components/carry_lookahead.v
+++ src/components/carry_lookahead.v
@@ -1,31 +1,58 @@
 module carry_lookahead(
-    input  [3:0] a,
-    input  [3:0] b,
-    input        carry_in,
-    output [3:0] result,
+    input  [7:0] a,
+    input  [7:0] b,
+    output [7:0] result,
     output       carry_out
 );
-wire [3:0] carry, propogate;
+wire [7:0] carry, propogate;
 
 assign carry     = a & b;
 assign propogate = a ^ b;
 
-assign result[0] = propogate[0] ^ carry_in;
+assign result[0] = propogate[0];
 
-assign result[1] = propogate[1] ^ 
-                   (carry[0] | (propogate[0] & carry_in));
+assign result[1] = propogate[1] ^ carry[0];
 
-assign result[2] = propogate[2] ^
-                   (carry[1] | (propogate[1] & 
-                     (carry[0] | propogate[0] & carry_in)));
+assign result[2] = propogate[2] ^ (carry[1] | 
+                   (propogate[1] & carry[0]));
 
-assign result[3] = propogate[3] ^
-                   (carry[2] | (propogate[2] & 
-                    (carry[1] | (propogate[1] & 
-                     (carry[0] | propogate[0] & carry_in)))));
+assign result[3] = propogate[3] ^ (carry[2] | 
+                    (propogate[2] & (carry[1] | 
+                     (propogate[1] & carry[0]))));
 
-assign carry_out = carry[3] | (propogate[3] & 
-                    (carry[2] | (propogate[2] & 
-                     (carry[1] | (propogate[1] & 
-                      (carry[0] | propogate[0] & carry_in))))));
+assign result[4] = propogate[4] ^ (carry[3] | 
+                    (propogate[3] & (carry[2] | 
+                     (propogate[2] & (carry[1] | 
+                      (propogate[1] & carry[0]))))));
+
+assign result[5] = propogate[5] ^ (carry[4] | 
+                    (propogate[4] & (carry[3] | 
+                     (propogate[3] & (carry[2] | 
+                      (propogate[2] & (carry[1] | 
+                       (propogate[1] & carry[0]))))))));
+
+assign result[6] = propogate[6] ^ (carry[5] | 
+                    (propogate[5] & (carry[4] | 
+                     (propogate[4] & (carry[3] | 
+                      (propogate[3] & (carry[2] | 
+                       (propogate[2] & (carry[1] | 
+                        (propogate[1] & carry[0]))))))))));
+
+assign result[7] = propogate[7] ^ (carry[6] | 
+                    (propogate[6] & (carry[5] | 
+                     (propogate[5] & (carry[4] | 
+                      (propogate[4] & (carry[3] | 
+                       (propogate[3] & (carry[2] | 
+                        (propogate[2] & (carry[1] | 
+                         (propogate[1] & carry[0]))))))))))));
+ 
+assign carry_out = carry[7] | 
+                    (propogate[7] & (carry[6] | 
+                     (propogate[6] & (carry[5] | 
+                      (propogate[5] & (carry[4] | 
+                       (propogate[4] & (carry[3] | 
+                        (propogate[3] & (carry[2] | 
+                         (propogate[2] & (carry[1] | 
+                          (propogate[1] & carry[0])))))))))))));
+
 endmodule
