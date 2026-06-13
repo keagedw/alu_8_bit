0d6d2695f93bf0f1f332d73e822e7af6da5b6b66
Keagan Edward Wangsakentjana <keagedw.official@gmail.com>
2026-06-13 22:38:59 +0800

    Clean carry_lookahead

--- src/components/carry_lookahead.v
+++ src/components/carry_lookahead.v
@@ -5,63 +5,71 @@ module carry_lookahead(
     output [7:0] result,
     output       carry_out
 );
-wire [7:0] carry, propogate;
+wire [7:0] carry_generate, carry_propagate;
+wire [6:0] carry;
 
-assign carry     = a & b;
-assign propogate = a ^ b;
+assign carry_generate  = a & b;
+assign carry_propagate = a ^ b;
 
-assign result[0] = propogate[0] ^ carry_in;
+assign carry[0] = carry_generate[0] | 
+                  (carry_propagate[0] & carry_in);
 
-assign result[1] = propogate[1] ^ (carry[0] | 
-                    (propogate[0] & carry_in));
+assign carry[1] = carry_generate[1] | 
+                   (carry_propagate[1] & (carry_generate[0] | 
+                    (carry_propagate[0] & carry_in)));
 
-assign result[2] = propogate[2] ^ (carry[1] | 
-                   (propogate[1] & (carry[0] | 
-                    (propogate[0] & carry_in))));
+assign carry[2] = carry_generate[2] | 
+                    (carry_propagate[2] & (carry_generate[1] | 
+                     (carry_propagate[1] & (carry_generate[0] | 
+                      (carry_propagate[0] & carry_in)))));
 
-assign result[3] = propogate[3] ^ (carry[2] | 
-                    (propogate[2] & (carry[1] | 
-                     (propogate[1] & (carry[0] | 
-                      (propogate[0] & carry_in))))));
+assign carry[3] = carry_generate[3] | 
+                    (carry_propagate[3] & (carry_generate[2] | 
+                     (carry_propagate[2] & (carry_generate[1] | 
+                      (carry_propagate[1] & (carry_generate[0] | 
+                       (carry_propagate[0] & carry_in)))))));
 
-assign result[4] = propogate[4] ^ (carry[3] | 
-                    (propogate[3] & (carry[2] | 
-                     (propogate[2] & (carry[1] | 
-                      (propogate[1] & (carry[0] | 
-                       (propogate[0] & carry_in))))))));
+assign carry[4] = carry_generate[4] | 
+                    (carry_propagate[4] & (carry_generate[3] | 
+                     (carry_propagate[3] & (carry_generate[2] | 
+                      (carry_propagate[2] & (carry_generate[1] | 
+                       (carry_propagate[1] & (carry_generate[0] | 
+                        (carry_propagate[0] & carry_in)))))))));
 
-assign result[5] = propogate[5] ^ (carry[4] | 
-                    (propogate[4] & (carry[3] | 
-                     (propogate[3] & (carry[2] | 
-                      (propogate[2] & (carry[1] | 
-                       (propogate[1] & (carry[0] | 
-                        (propogate[0] & carry_in))))))))));
+assign carry[5] = carry_generate[5] | 
+                    (carry_propagate[5] & (carry_generate[4] | 
+                     (carry_propagate[4] & (carry_generate[3] | 
+                      (carry_propagate[3] & (carry_generate[2] | 
+                       (carry_propagate[2] & (carry_generate[1] | 
+                        (carry_propagate[1] & (carry_generate[0] | 
+                         (carry_propagate[0] & carry_in)))))))))));
 
-assign result[6] = propogate[6] ^ (carry[5] | 
-                    (propogate[5] & (carry[4] | 
-                     (propogate[4] & (carry[3] | 
-                      (propogate[3] & (carry[2] | 
-                       (propogate[2] & (carry[1] | 
-                        (propogate[1] & (carry[0] | 
-                         (propogate[0] & carry_in))))))))))));
+assign carry[6] = carry_generate[6] | 
+                    (carry_propagate[6] & (carry_generate[5] | 
+                     (carry_propagate[5] & (carry_generate[4] | 
+                      (carry_propagate[4] & (carry_generate[3] | 
+                       (carry_propagate[3] & (carry_generate[2] | 
+                        (carry_propagate[2] & (carry_generate[1] | 
+                         (carry_propagate[1] & (carry_generate[0] | 
+                          (carry_propagate[0] & carry_in)))))))))))));
 
-assign result[7] = propogate[7] ^ (carry[6] | 
-                    (propogate[6] & (carry[5] | 
-                     (propogate[5] & (carry[4] | 
-                      (propogate[4] & (carry[3] | 
-                       (propogate[3] & (carry[2] | 
-                        (propogate[2] & (carry[1] | 
-                         (propogate[1] & (carry[0] | 
-                          (propogate[0] & carry_in))))))))))))));
- 
-assign carry_out = carry[7] | 
-                    (propogate[7] & (carry[6] | 
-                     (propogate[6] & (carry[5] | 
-                      (propogate[5] & (carry[4] | 
-                       (propogate[4] & (carry[3] | 
-                        (propogate[3] & (carry[2] | 
-                         (propogate[2] & (carry[1] | 
-                          (propogate[1] & (carry[0] | 
-                           (propogate[0] & carry_in)))))))))))))));
+assign carry_out = carry_generate[7] | 
+                    (carry_propagate[7] & (carry_generate[6] | 
+                     (carry_propagate[6] & (carry_generate[5] | 
+                      (carry_propagate[5] & (carry_generate[4] | 
+                       (carry_propagate[4] & (carry_generate[3] | 
+                        (carry_propagate[3] & (carry_generate[2] | 
+                         (carry_propagate[2] & (carry_generate[1] | 
+                          (carry_propagate[1] & (carry_generate[0] | 
+                           (carry_propagate[0] & carry_in)))))))))))))));
+
+assign result[0] = carry_propagate[0] ^ carry_in;
+assign result[1] = carry_propagate[1] ^ (carry[0]);
+assign result[2] = carry_propagate[2] ^ (carry[1]);
+assign result[3] = carry_propagate[3] ^ (carry[2]);
+assign result[4] = carry_propagate[4] ^ (carry[3]);
+assign result[5] = carry_propagate[5] ^ (carry[4]);
+assign result[6] = carry_propagate[6] ^ (carry[5]);
+assign result[7] = carry_propagate[7] ^ (carry[6]);
 
 endmodule
